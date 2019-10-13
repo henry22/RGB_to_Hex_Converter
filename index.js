749589ffe8d0cdeac49853b90b0c714f31eba1a7
henry22 <henry811010@gmail.com>
2019-10-13 21:20:58 +0800

    listen input event to each slider bar and create rgbToHex function as a handler

--- index.js
+++ index.js
@@ -11,4 +11,43 @@
   let hexColor = '#000000'
 
   container.style.background = hexColor
+
+  function rgbToHex(event) {
+    let sliderValue = 0
+
+    switch (event.target.id) {
+      case 'red': 
+        sliderValue = Number(event.target.value)
+
+        redSliderValue.innerHTML = sliderValue
+        redHexColor = hexConverter(sliderValue)
+        break
+      case 'green':
+        sliderValue = Number(event.target.value)
+
+        greenSliderValue.innerHTML = sliderValue
+        greenHexColor = hexConverter(sliderValue)
+        break
+      case 'blue':
+        sliderValue = Number(event.target.value)
+
+        blueSliderValue.innerHTML = sliderValue
+        blueHexColor = hexConverter(sliderValue)
+        break
+    }
+
+    hexColor = `#${redHexColor}${greenHexColor}${blueHexColor}`
+
+    hexValue.innerHTML = hexColor
+    container.style.background = hexColor
+  }
+
+  function hexConverter(rgbValue) {
+    let hex = rgbValue.toString(16)
+    return hex.length === 1 ? '0' + hex : hex
+  }
+
+  for (let i = 0; i < sliders.length; i++) {
+    sliders[i].addEventListener('input', rgbToHex)
+  }
 })()
